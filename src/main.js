23af5aeb13a857ccfec87eaeef20b99a3e79d179
loflollop <13522070865@163.com>
2018-11-01 01:26:54 +0800

    登录页面渲染

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import App from './App'
 import router from './router'
+import '@/assets/css/index.css'
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
